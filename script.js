7873a07ea7234f9e2430d6215117303242674b91
satvarika <satvarika@gmail.com>
2021-11-03 20:46:44 +0300

    first

---
+++ script.js
@@ -0,0 +1,26 @@
+let bear = {
+    color: 'white',
+    birthDay: new Date('1089-08-18T03:24:00'),
+    eat: {
+        honey: true,
+        bunny: false,
+    },
+
+}
+
+function getBearColor() {
+    document.getElementById("bearColor").innerHTML = bear.color
+}
+
+// console.log('мишута какой цвет?', bear.color)
+
+// console.log('мишута есть зайчат?', bear.eat.bunny)
+
+// console.log('мишута есть мед?', bear.eat.honey)
+
+
+// const diffTime = new Date() - bear.birthDay
+// const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24 * 365))
+
+// console.log('сколько лет?', diffDays)
+
